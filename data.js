0d478ff5b6c5b0b0e332f6b5ecc26c7b36cca0cf
Sopels <jacekkwakauni@gmail.com>
2025-02-18 13:07:40 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,73 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama-1",
+      "name": "panorama (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.4555887526033509,
+          "pitch": 0.09541519202016424,
+          "title": "Hall 3201",
+          "text": "<div><div><p class=\"MsoNormal\"><span style=\"font-size:8.0pt;line-height:115%\">Zaawansowany\nsymulator dorosłego pacjenta przeznaczony do nauki, treningu szeroko pojętych\nzabiegów i badań diagnostycznych przeprowadzanych przez lekarzy, pielęgniarek\noraz ratowników medycznych w warunkach klinicznych.&nbsp;</span></p></div></div>"
+        },
+        {
+          "yaw": -1.2960216173072396,
+          "pitch": -0.040959015681760746,
+          "title": "Emtel Defimax Plus",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size:8.0pt;line-height:115%\">Defibrylator\numożliwia monitorowanie EKG, ciśnienia tętniczego mierzonego metodą\nnieinwazyjną (NIBP) i inwazyjną (IBP), saturacji (SpO2), temperatury oraz\ndwutlenku węgla (CO2).<o:p></o:p></span></p>"
+        },
+        {
+          "yaw": -1.6876881778497363,
+          "pitch": 0.11905722388754292,
+          "title": "EKG CardioCor",
+          "text": "<span style=\"font-size:8.0pt;line-height:115%;\nfont-family:&quot;Aptos&quot;,sans-serif;mso-ascii-theme-font:minor-latin;mso-fareast-font-family:\nAptos;mso-fareast-theme-font:minor-latin;mso-hansi-theme-font:minor-latin;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-bidi-theme-font:minor-bidi;\nmso-ansi-language:PL;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">Przenośny\n12 kanałowy aparat EKG. Jednoczesny zapis 3, 6 lub 12 kanałów EKG. Ciągły\npomiar i wyświetlanie częstości rytmu serca (HR). Automatyczna analiza i\ninterpretacja HES</span>"
+        },
+        {
+          "yaw": -1.4354743135488306,
+          "pitch": 0.3685686044342482,
+          "title": "Stół Operacyjny",
+          "text": "<span style=\"font-size:8.0pt;line-height:115%;\nfont-family:&quot;Aptos&quot;,sans-serif;mso-ascii-theme-font:minor-latin;mso-fareast-font-family:\nAptos;mso-fareast-theme-font:minor-latin;mso-hansi-theme-font:minor-latin;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-bidi-theme-font:minor-bidi;\nmso-ansi-language:PL;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">Uniwersalny\nstół operacyjny z elektro-hydrauliczną regulacją za pomocą pilota zdalnego\nsterowania dla trzech różnych ustawień: – góra / dół, – przechyły boczne (lewa\n/ prawa) – pozycja Trendelenburga / anty-Trendelenburga..&nbsp;</span>"
+        },
+        {
+          "yaw": -1.7542501398916865,
+          "pitch": -0.12251404446675807,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size:11.0pt;line-height:115%\">Pompa Infuzyjna Medima P200<o:p></o:p></span></p>",
+          "text": "<p class=\"MsoNormal\" style=\"margin-bottom:0cm\"><span style=\"font-size:8.0pt;line-height:115%;\nfont-family:&quot;Aptos&quot;,sans-serif;mso-ascii-theme-font:minor-latin;mso-fareast-font-family:\nAptos;mso-fareast-theme-font:minor-latin;mso-hansi-theme-font:minor-latin;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-bidi-theme-font:minor-bidi;\nmso-ansi-language:PL;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">Model\npodstawowy&nbsp;Infuzja ciągła w mL/h.&nbsp;Wyliczanie szybkości infuzji na\npodstawie objętości i czasu.&nbsp;Kilka sekund od momentu instalacji strzykawki\ndo uruchomienia infuzji</span></p>"
+        }
+      ]
+    }
+  ],
+  "name": "WCSM BLOK",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
